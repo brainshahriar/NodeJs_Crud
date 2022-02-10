23930a318dcfdc39a032fcb510280b0ef8efae1e
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-11 00:53:25 +0600

    testful

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
+const dotenv = require('dotenv');
+
+const morgan = require('morgan');
+
+const bodyParser = require('body-parser');
+
+//log request
+
+app.use(morgan('tiny'));
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+dotenv.config({path:'.env'});
+
+const PORT = process.env.PORT || 8080
+
 app.get('/',(req,res)=>{
     res.send("hello crud");
 })
 
-app.listen(3000,()=>{console.log(`Server is running on http://localhost:${3000}`)});
+app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
